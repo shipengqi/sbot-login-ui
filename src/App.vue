21bce556541bf33d3f053eb9afafd2e5dbd4bbb9
shipengqi <pooky.shipengqi@gmail.com>
2019-07-14 19:18:17 +0800

    ui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,163 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="login">
+      <div id="description">
+        <h1>Login</h1>
+        <p>By logging in you agree to the ridiculously long terms that you didn't bother to read.</p>
+      </div>
+      <div id="form">
+        <form @submit.prevent="doLogin">
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="username" placeholder="username" autocomplete="off">
+
+          <label for="password">Password</label>&nbsp;
+          <i class="fas" :class="[passwordIcon]" @click="hidePassword = !hidePassword"></i>
+          <input type="password" id="password" v-model="password" placeholder="password">
+          <!--<el-button type="primary" @click="submitForm('ruleForm')">Log in</el-button>-->
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      ruleForm: {
+        pass: '',
+        checkPass: ''
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      console.log('submit!!')
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    font-family: 'Nunito', sans-serif;
+  }
+
+  html,
+  body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+  }
+
+  div#app {
+    width: 100%;
+    height: 100%;
+  }
+
+  div#app div#login {
+    align-items: center;
+    background-color: #e2e2e5;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  div#app div#login div#description {
+    background-color: #ffffff;
+    width: 25%;
+    height: 25%;
+    padding: 35px;
+  }
+
+  div#app div#login div#description h1,
+  div#app div#login div#description p {
+    margin: 0;
+  }
+
+  div#app div#login div#description p {
+    font-size: 0.8em;
+    color: #95a5a6;
+    margin-top: 10px;
+  }
+
+  div#app div#login div#form {
+    background-color: #34495e;
+    border-radius: 5px;
+    box-shadow: 0px 0px 30px 0px #666;
+    color: #ecf0f1;
+    width: 25%;
+    height: 35%;
+    padding: 35px;
+  }
+
+  div#app div#login div#form label,
+  div#app div#login div#form input {
+    outline: none;
+    width: 100%;
+  }
+
+  div#app div#login div#form label {
+    color: #95a5a6;
+    font-size: 0.8em;
+  }
+
+  div#app div#login div#form input {
+    background-color: transparent;
+    border: none;
+    color: #ecf0f1;
+    font-size: 1em;
+    margin-bottom: 20px;
+  }
+
+  div#app div#login div#form ::placeholder {
+    color: #ecf0f1;
+    opacity: 1;
+  }
+
+  div#app div#login div#form button {
+    background-color: #ffffff;
+    cursor: pointer;
+    border: none;
+    padding: 10px;
+    transition: background-color 0.2s ease-in-out;
+    width: 100%;
+  }
+
+  div#app div#login div#form button:hover {
+    background-color: #eeeeee;
+  }
+
+  @media screen and (max-width: 600px) {
+    div#app div#login {
+      align-items: unset;
+      background-color: unset;
+      display: unset;
+      justify-content: unset;
+    }
+
+    div#app div#login div#description {
+      margin: 0 auto;
+      max-width: 25%;
+      width: 100%;
+    }
+
+    div#app div#login div#form {
+      border-radius: unset;
+      box-shadow: unset;
+      width: 100%;
+    }
+
+    div#app div#login div#form form {
+      margin: 0 auto;
+      max-width: 25%;
+      width: 100%;
+    }
+  }
 </style>
